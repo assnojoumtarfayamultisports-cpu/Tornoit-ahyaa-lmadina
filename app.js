8be4fe5bc3009375d74b3c796c7ce0ba8db39c0d
assnojoumtarfayamultisports-cpu <ass.nojoum.tarfaya.multisports@gmail.com>
2026-06-24 16:21:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+document.body.innerHTML = `
+<div class="header">
+    <h1>بطولة أحياء المدينة لكرة القدم المصغرة 2026</h1>
+    <p>جمعية نجوم طرفاية متعددة الرياضات</p>
+</div>
+
+<div class="container">
+
+<div class="groups">
+
+<div class="card">
+<h2>المجموعة A</h2>
+<ol>
+<li>اتحاد جيلي</li>
+<li>نادي الصداقة</li>
+<li>فريق الإنبعاث</li>
+<li>شباب طرفاية</li>
+<li>الحي الجديد</li>
+</ol>
+</div>
+
+<div class="card">
+<h2>المجموعة B</h2>
+<ol>
+<li>انتر طرفاية</li>
+<li>حي التقدم</li>
+<li>الحي الشرقي</li>
+<li>اتحاد لمكيزرة</li>
+<li>حي المحيط</li>
+</ol>
+</div>
+
+</div>
+
+<br>
+
+<table>
+<thead>
+<tr>
+<th>اليوم</th>
+<th>التاريخ</th>
+<th>المجموعة</th>
+<th>المباراة</th>
+</tr>
+</thead>
+
+<tbody>
+<tr><td>الإثنين</td><td>22/06/2026</td><td>A</td><td>اتحاد جيلي × نادي الصداقة</td></tr>
+<tr><td>الثلاثاء</td><td>23/06/2026</td><td>B</td><td>انتر طرفاية × حي التقدم</td></tr>
+<tr><td>الأربعاء</td><td>24/06/2026</td><td>A</td><td>فريق الإنبعاث × اتحاد جيلي</td></tr>
+<tr><td>الخميس</td><td>25/06/2026</td><td>B</td><td>الحي الشرقي × انتر طرفاية</td></tr>
+<tr><td>الجمعة</td><td>26/06/2026</td><td>A</td><td>شباب طرفاية × اتحاد جيلي</td></tr>
+<tr><td>السبت</td><td>27/06/2026</td><td>B</td><td>اتحاد لمكيزرة × انتر طرفاية</td></tr>
+<tr><td>الأحد</td><td>28/06/2026</td><td>A</td><td>الحي الجديد × اتحاد جيلي</td></tr>
+<tr><td>الإثنين</td><td>29/06/2026</td><td>B</td><td>حي المحيط × انتر طرفاية</td></tr>
+</tbody>
+
+</table>
+
+</div>
+`;
+
+});
